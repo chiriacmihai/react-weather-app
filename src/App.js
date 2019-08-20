61149f4fd51c209e996ea44c2e873f819211db28
NemoS <chiriacmihai@github.com>
2019-08-21 02:00:10 +0300

    First change

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           My First React Project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.accuweather.com/en/md/chiinu/242405/weather-forecast/242405"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Weather
         </a>
       </header>
     </div>
